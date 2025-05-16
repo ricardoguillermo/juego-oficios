c8344fed3e77a340fe5262aa45849030c469c10d
Ricardo Nario <diguibal@gmail.com>
2025-05-16 08:05:54 -0300

    Cambie el vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    allowedHosts: ['juego-oficios.onrender.com'],
+    host: "0.0.0.0", // 👈 clave para Render
+    port: 3000,
   },
 });
